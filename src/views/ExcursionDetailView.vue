71d562b1bccf87a9cc7316bdafdb63e287d1ffef
Константин <mr.chernomorec@mail.ru>
2026-08-30 17:09:45 +0300

    Переход на туры вручную

--- src/views/ExcursionDetailView.vue
+++ src/views/ExcursionDetailView.vue
@@ -1,3 +1,30 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { excursions } from '../data/excursions';
+
+
+const route = useRoute();
+
+const excursion = excursions.find(
+    excursion => excursion.id === Number(route.params.id)
+);
+
+console.log(route.params.id);
+</script>
+
+
 <template>
-  <h1>Описание экскурсии</h1>
+  <div>
+    <h1>{{ excursion.title }}</h1>
+
+    <img
+      :src="excursion.image"
+      :alt="excursion.title"
+    >
+
+    <p>{{ excursion.description }}</p>
+
+    <p>{{ excursion.adultPrice }} ₽</p>
+    <p>{{ excursion.childPrice }} ₽</p>
+  </div>
 </template>
