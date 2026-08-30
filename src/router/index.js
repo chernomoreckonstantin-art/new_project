4eee354d998492025b8772311494da8680834f7f
Константин <mr.chernomorec@mail.ru>
2026-08-30 16:12:07 +0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ExcursionsView from "../views/ExcursionsView.vue";
+import ExcursionDetailView from "../views/ExcursionDetailView.vue";
 
 const routes = [
     {
         path: '/',
         component : ExcursionsView
+    },
+    {
+        path: '/tours/:id',
+        component : ExcursionDetailView
     }
 
 ]
